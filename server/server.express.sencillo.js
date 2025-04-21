9bd64dbd01c41ffbda00ae72d495acd3dd9248d7
davidglnd <david.galindo.dgp@gmail.com>
2025-04-21 15:53:27 +0200

    chore: express

---
+++ server/server.express.sencillo.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { crud } from "./server.crud.js";
+
+const app = express();
+const port = process.env.PORT;
+const USERS_URL = './server/BBDD/users.json'
+const CLUBS_URL = './server/BBDD/clubes.json'
+
+// Static server
+app.use(express.static('src'));
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Router de la aplicación
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// API ENDPOINTS
+app.get('/read/users', (req, res) => {
+  crud.read(USERS_URL, (data) => {
+    console.log('servidor leyendo usuarios del json', data)
+
+    res.send(JSON.stringify(data));
+  });
+});
+
+app.get('/read/clubs', (req, res) => {
+  crud.read(CLUBS_URL, (data) => {
+    console.log('servidor leyendo clubes del json', data) 
+    res.send(JSON.stringify(data));
+  });
+})
+
+app.post('/create/users', (req, res) => {
+  crud.create(USERS_URL, req.body, (data) => {
+    console.log(`server create user ${data.name} creado`, data)
+    res.send(JSON.stringify(data));
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
