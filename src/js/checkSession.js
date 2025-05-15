43a96dd80c46786e84a82d667b2c70332b8f2ab1
davidglnd <david.galindo.dgp@gmail.com>
2025-05-15 13:44:42 +0200

    feat(CSS): :sparkles: CSS de index
    CSS de index empezado aun queda retocar mucho pero ya sigue la linea, queda darle un poco mas de estilos ya que es la landing page, desarrollar contenidos de pestañas etc, he movido tambien el boton de inicio de sesion y lo he separado del registro(que aun habra que moverlo))

--- src/js/checkSession.js
+++ src/js/checkSession.js
@@ -1,9 +1,14 @@
 export function comprobarSession(){
     if(sessionStorage.getItem('HOOP_MANAGER') !== null){
         console.log('estas login')
+        return
     }else if(sessionStorage.getItem('HOOP_MANAGER_CLUB') !== null && location.pathname === '/admin-club.html'){
         console.log('estas en el admin')
-    }else if (location.pathname !== '/index.html') {
+        return
+    }else if(location.pathname === '/login.html'){
+        console.log('pantalla login')
+        return
+    }else if (location.pathname !== '/index.html' ) {
         // Redirigimos a la home si el usuario no está identificado
         location.href = '/index.html'
     }
--- src/js/logic/gestion-usuarios-script.js
+++ src/js/logic/gestion-usuarios-script.js
@@ -17,33 +17,14 @@ export const API_PORT = location.port ? `:${1337}` : ''
  * - Logs 'Todo cargado' to the console.
  */
 function onDOMContentLoaded() {
-    let botonSignIn = document.getElementById('boton-sign-in')
-    let botonLogIn = document.getElementById('boton-log-in')
-
     let mostrarSignUsuario = document.getElementById('registrarse-usuario')
     let mostrarSignClub = document.getElementById('registrarse-club')
 
     mostrarSignUsuario?.addEventListener('click', mostrarSignInUsuario)
     mostrarSignClub?.addEventListener('click', mostrarSignInClub)
 
-    botonLogIn?.addEventListener('click', mostrarLogIn)
-    botonSignIn?.addEventListener('click', mostrarSignIn)
-
     comprobarSession()
     console.log('Todo cargado')
-}
-function mostrarLogIn(){
-    document.getElementById('comp-log-in')?.classList.remove('hidden')
-    document.getElementById('contenedor-sign-in')?.classList.add('hidden')
-    document.getElementById('boton-log-in')?.classList.add('hidden')
-    document.getElementById('boton-sign-in')?.classList.add('hidden')
-}
-
-function mostrarSignIn(){
-    document.getElementById('contenedor-sign-in')?.classList.remove('hidden')
-    document.getElementById('contenedor-log-in')?.classList.add('hidden')
-    document.getElementById('boton-log-in')?.classList.add('hidden')
-    document.getElementById('boton-sign-in')?.classList.add('hidden')
 }
 function mostrarSignInClub(){
     document.getElementById('comp-sign-in-club')?.classList.remove('hidden')
