1899ca15fc0595bb88baa94cb803be413448b3e9
davidglnd <david.galindo.dgp@gmail.com>
2025-05-15 17:46:31 +0200

    Update

--- src/js/checkSession.js
+++ src/js/checkSession.js
@@ -1,15 +1,16 @@
 export function comprobarSession(){
-    if(sessionStorage.getItem('HOOP_MANAGER') !== null){
-        console.log('estas login')
-        return
-    }else if(sessionStorage.getItem('HOOP_MANAGER_CLUB') !== null && location.pathname === '/admin-club.html'){
-        console.log('estas en el admin')
-        return
-    }else if(location.pathname === '/login.html'){
-        console.log('pantalla login')
-        return
-    }else if (location.pathname !== '/index.html' ) {
-        // Redirigimos a la home si el usuario no está identificado
-        location.href = '/index.html'
-    }
+    // if(sessionStorage.getItem('HOOP_MANAGER') !== null){
+    //     console.log('estas login')
+    //     return
+    // }else if(sessionStorage.getItem('HOOP_MANAGER_CLUB') !== null && location.pathname === '/admin-club.html'){
+    //     console.log('estas en el admin')
+    //     return
+    // }else if(location.pathname === '/login.html'){
+    //     console.log('pantalla login')
+    //     return
+    // }else if (location.pathname !== '/index.html' ) {
+    //     // Redirigimos a la home si el usuario no está identificado
+    //     location.href = '/index.html'
+    // }
+    console.log('CHECKSESSION EN DESARROLLO')
 }
